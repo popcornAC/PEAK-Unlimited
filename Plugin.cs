0f7f02d8aef426bfd99f47a62c5d8e76161c3bad
Lewis <hallrudfordlewis@gmail.com>
2025-06-17 15:12:06 +0100

    Add configuration
    Users can now set their own max lobby size

--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using BepInEx.Configuration;
 using HarmonyLib;
 using Steamworks;
 using Photon.Realtime;
@@ -10,8 +11,8 @@ namespace PeakUnlimited;
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-    private static int NEW_MAX_PLAYERS = 10;
-    
+    private static int NEW_MAX_PLAYERS;
+    private static ConfigEntry<int> configMaxPlayers;
     private readonly Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
         
     private void Awake()
@@ -19,6 +20,16 @@ public class Plugin : BaseUnityPlugin
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        
+        configMaxPlayers = Config.Bind
+        (
+            "General", 
+            "MaxPlayers", 
+            10, 
+            "The maximum number of players you want to be able to join your lobby. Warning: untested, higher numbers may break!"
+        );
+        
+        NEW_MAX_PLAYERS = configMaxPlayers.Value;
         
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is patching!");
         harmony.PatchAll(typeof(HostRoomOptionsPatch));
