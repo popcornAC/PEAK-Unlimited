b383da1fbe8d529d8f1eb8f1a28547bd4e94ac2c
Lewis <hallrudfordlewis@gmail.com>
2025-06-22 04:06:00 +0100

    Marshmallow fixes + csproj

--- Plugin.cs
+++ Plugin.cs
@@ -22,10 +22,14 @@ namespace PeakUnlimited;
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-    private static int NEW_MAX_PLAYERS;
-    private static ConfigEntry<int> configMaxPlayers;
-    private static ConfigEntry<bool> configExtraMarshmallows;
-    private readonly Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+    private static int _newMaxPlayers;
+    private static int _cheatExtraMarshmallows;
+    private static bool _extraMarshmallows;
+    private static ConfigEntry<int> _configMaxPlayers;
+    private static ConfigEntry<int> _configCheatExtraMarshmallows;
+    private static ConfigEntry<bool> _configExtraMarshmallows;
+    private static int _numberOfPlayers = 1;
+    private readonly Harmony _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
 
     private void Awake()
     {
@@ -33,40 +37,54 @@ public class Plugin : BaseUnityPlugin
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
 
-        configMaxPlayers = Config.Bind
+        _configMaxPlayers = Config.Bind
         (
             "General",
             "MaxPlayers",
             20,
             "The maximum number of players you want to be able to join your lobby (Including yourself). Warning: untested, higher numbers may be unstable! Range: 1-20"
         );
+        _newMaxPlayers = _configMaxPlayers.Value;
         
-        configExtraMarshmallows = Config.Bind
+        _configExtraMarshmallows = Config.Bind
         (
             "General",
             "ExtraMarshmallows",
             true,
             "Controls whether additional marshmallows are spawned for the extra players (Experimental)"
         );
+        _extraMarshmallows = _configExtraMarshmallows.Value;
+        
 
-        NEW_MAX_PLAYERS = configMaxPlayers.Value;
-
-        if (NEW_MAX_PLAYERS == 0)
+        _configCheatExtraMarshmallows = Config.Bind
+        (
+            "General",
+            "Cheat Marshmallows",
+            0,
+            "(Cheat, disabled by default) This will set the desired amount of marshmallows to the campfires as a cheat, requires ExtraMarshmallows to be enabled."
+        );
+        _cheatExtraMarshmallows = _configCheatExtraMarshmallows.Value;
+        
+        if (_newMaxPlayers == 0)
         {
-            NEW_MAX_PLAYERS = 1;
+            _newMaxPlayers = 1;
         }
-        else if (NEW_MAX_PLAYERS > 30)
+        else if (_newMaxPlayers > 30)
         {
-            NEW_MAX_PLAYERS = 30;
+            _newMaxPlayers = 30;
         }
 
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} set the Max Players to " + NEW_MAX_PLAYERS + "!");
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} set the Max Players to " + _newMaxPlayers + "!");
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is patching!");
-        harmony.PatchAll(typeof(HostRoomOptionsPatch));
-        harmony.PatchAll(typeof(PlayClickedPatch));
-        if (configExtraMarshmallows.Value) {
+        _harmony.PatchAll(typeof(HostRoomOptionsPatch));
+        _harmony.PatchAll(typeof(PlayClickedPatch));
+        if (_extraMarshmallows) {
             Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} extra marshmallows are enabled!");
-            harmony.PatchAll(typeof(AwakePatch));
+            _harmony.PatchAll(typeof(AwakePatch));
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} left patch enabled!");
+            _harmony.PatchAll(typeof(OnPlayerLeftRoomPatch));
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} joined patch enabled!");
+            _harmony.PatchAll(typeof(OnPlayerEnteredRoomPatch));
         }
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} has patched!");
     }
@@ -86,34 +104,52 @@ public class Plugin : BaseUnityPlugin
             float angle = i * Mathf.PI * 2f / numPoints; // Even spacing: 2π / n
             float x = radius * Mathf.Cos(angle);
             float z = radius * Mathf.Sin(angle);
-            points.Add(setToGround(new Vector3(x, 0f, z) + campfirePosition));
+            points.Add(SetToGround(new Vector3(x, 0f, z) + campfirePosition));
         }
-
+        
         return points;
     }
-
-    private static Vector3 setToGround(Vector3 vector)
+    
+    private static List<Vector3> getCampfirePositions()
     {
-        return HelperFunctions.GetGroundPos(vector, HelperFunctions.LayerType.Terrain);
-    }
-
-    private void spawnMarshmallows(int number)
-    {
-        Vector3 campfirePosition = GameObject.Find("Campfire").transform.position;
-        ItemDatabase itemDatabase = SingletonAsset<ItemDatabase>.Instance;
-        Item marshmallowItem = itemDatabase.itemLookup[46];
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} " + marshmallowItem.GetName());
-        marshmallowItem.GetName();
-        List<Vector3> marshmallowPositions = GetEvenlySpacedPointsAroundCampfire(number, 2.5f, 3, campfirePosition);
-        foreach (Vector3 marshmallowPosition in marshmallowPositions)
+        List<Vector3> campfirePositions = new List<Vector3>();
+        foreach (Campfire campfire in ((IEnumerable<Campfire>) Resources.FindObjectsOfTypeAll<Campfire>()).Where<Campfire>((Func<Campfire, bool>) (c => c.gameObject.scene.IsValid())).ToArray<Campfire>())
         {
-            Add(marshmallowItem, marshmallowPosition);
+            Plugin.Logger.LogInfo((object) "Found campfire!");
+            campfirePositions.Add(campfire.gameObject.transform.position);
         }
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} added with position: " + marshmallowItem.GetName());
+        return campfirePositions;
     }
     
     private static void spawnMarshmallows(int number, Vector3 campfirePosition)
     {
+        Item obj = SingletonAsset<ItemDatabase>.Instance.itemLookup[(ushort) 46];
+        Plugin.Logger.LogInfo((object) ("Plugin PeakUnlimited " + obj.GetName()));
+        obj.GetName();
+        foreach (Vector3 position in Plugin.GetEvenlySpacedPointsAroundCampfire(number, 2.5f, 3f, campfirePosition))
+            Plugin.Add(obj, position);
+        Plugin.Logger.LogInfo((object) ("Plugin PeakUnlimited added with position: " + obj.GetName()));
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.U))
+        {
+            foreach (Vector3 campfirePosition in Plugin.getCampfirePositions())
+            {
+                Plugin.Logger.LogInfo((object)"Spawning campfire mms!");
+                Plugin.spawnMarshmallows(4, campfirePosition);
+            }
+        }
+    }
+
+    private static Vector3 SetToGround(Vector3 vector)
+    {
+        return HelperFunctions.GetGroundPos(vector, HelperFunctions.LayerType.Terrain);
+    }
+    
+    private static void SpawnMarshmallows(int number, Vector3 campfirePosition)
+    {
         ItemDatabase itemDatabase = SingletonAsset<ItemDatabase>.Instance;
         Item marshmallowItem = itemDatabase.itemLookup[46];
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} " + marshmallowItem.GetName());
@@ -124,36 +160,6 @@ public class Plugin : BaseUnityPlugin
             Add(marshmallowItem, marshmallowPosition);
         }
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} added with position: " + marshmallowItem.GetName());
-    }
-
-    private static List<Vector3> getCampfirePositions()
-    {
-        List<Vector3> positions = new List<Vector3>();
-        Campfire[] campfires = Resources.FindObjectsOfTypeAll<Campfire>()
-            .Where(c => c.gameObject.scene.IsValid())
-            .ToArray();
-        foreach (Campfire campfire in campfires)
-        {
-            Logger.LogInfo("Found campfire!");
-            positions.Add(campfire.gameObject.transform.position); 
-        }
-        return positions;
-    }
-    
-    private void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.Y))
-        {
-            spawnMarshmallows(20);
-        }
-        if (Input.GetKeyUp(KeyCode.U))
-        {
-            foreach (Vector3 campfirePosition in getCampfirePositions())
-            {
-                Logger.LogInfo("Spawning campfire mms!");
-                spawnMarshmallows(20, campfirePosition);
-            }
-        }
     }
     
     private static void Add(Item item, Vector3 position)
@@ -174,7 +180,7 @@ public class Plugin : BaseUnityPlugin
             __result = new RoomOptions()
             {
                 IsVisible = false,
-                MaxPlayers = NEW_MAX_PLAYERS
+                MaxPlayers = _newMaxPlayers
             };
         }
     }
@@ -185,7 +191,7 @@ public class Plugin : BaseUnityPlugin
         [HarmonyPrefix]
         static bool Prefix()
         {
-            SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, NEW_MAX_PLAYERS);
+            SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, _newMaxPlayers);
             return false;
         }
     }
@@ -196,15 +202,63 @@ public class Plugin : BaseUnityPlugin
         [HarmonyPostfix]
         static void Postfix(Campfire __instance)
         {
-            Logger.LogInfo("Start of campfire patch!");
-            if (PlayerHandler.GetAllPlayers().Count < 5) { return; }
-            
-            if (!__instance.gameObject.transform.parent.gameObject.name.ToLower().Contains("wings"))
+            if (__instance.gameObject.transform.parent.gameObject.name.ToLower().Contains("wings"))
             {
-                Vector3 campfirePosition = __instance.gameObject.transform.position;
-                //mmmm magic numbers
-                spawnMarshmallows(PlayerHandler.GetAllPlayers().Count+1-4+20, campfirePosition);
+                Logger.LogInfo("Skipping plane campfire...");
+                return;
             }
+            Logger.LogInfo("Marshmellowifying campfire...!");
+            int vanillaMaxPlayers = 4;
+            int currentPlayers = Plugin._numberOfPlayers;
+            int amountOfMarshmallowsToSpawn = currentPlayers - vanillaMaxPlayers;
+            if (_cheatExtraMarshmallows != 0)
+            {
+                Logger.LogInfo("Adding cheatmellows!");
+                amountOfMarshmallowsToSpawn = _cheatExtraMarshmallows - vanillaMaxPlayers;
+                if (currentPlayers < vanillaMaxPlayers)
+                {
+                    amountOfMarshmallowsToSpawn = _cheatExtraMarshmallows - currentPlayers + 1;
+                }
+            }
+            Plugin.Logger.LogInfo("Start of campfire patch!");
+            if (currentPlayers > vanillaMaxPlayers || _cheatExtraMarshmallows != 0)
+            {
+                Logger.LogInfo("More than 4 players, preparing to marshmallowify! Number: " + currentPlayers);
+                Vector3 position = __instance.gameObject.transform.position;
+                Logger.LogInfo("Spawning " + amountOfMarshmallowsToSpawn + " marshmallows!");
+                Plugin.spawnMarshmallows(amountOfMarshmallowsToSpawn, position);
+                Logger.LogInfo("End of campfire patch!");
+            }
+            else
+            {
+                Logger.LogInfo("Not enough players for campfire patch!");
+            }
+        }
+    }
+    
+    public class OnPlayerEnteredRoomPatch
+    {
+        [HarmonyPatch(typeof(PlayerConnectionLog), "OnPlayerEnteredRoom")]
+        [HarmonyPostfix]
+        static void Postfix(PlayerConnectionLog __instance)
+        {
+            _numberOfPlayers++;
+            Logger.LogInfo("Someone has joined the room! Number: " + _numberOfPlayers);
+        }
+    }
+    
+    public class OnPlayerLeftRoomPatch
+    {
+        [HarmonyPatch(typeof(PlayerConnectionLog), "OnPlayerLeftRoom")]
+        [HarmonyPostfix]
+        static void Postfix(PlayerConnectionLog __instance)
+        {
+            _numberOfPlayers--;
+            if (_numberOfPlayers < 0)
+            {
+                _numberOfPlayers = 0;
+            }
+            Logger.LogInfo("Someone has left the room! Number: " + _numberOfPlayers);
         }
     }
 }
