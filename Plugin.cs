e6b391dd4414315052b2c99bf2c5ffe294c4a26f
Lewis <hallrudfordlewis@gmail.com>
2025-07-07 02:02:57 +0100

    Add files via upload

--- Plugin.cs
+++ Plugin.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Reflection;
+﻿using System.Collections.Generic;
 using BepInEx;
-using BepInEx.Logging;
 using BepInEx.Configuration;
+using BepInEx.Logging;
 using HarmonyLib;
 using Photon.Pun;
-using Steamworks;
-using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.UI;
 using Zorro.Core;
-using Quaternion = UnityEngine.Quaternion;
-using Random = UnityEngine.Random;
-using Vector3 = UnityEngine.Vector3;
 
-namespace PeakUnlimited;
+namespace PEAKUnlimited;
 
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
+// Here are some basic resources on code style and naming conventions to help
+// you in your first CSharp plugin!
+// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
+// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/identifier-names
+// https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-namespaces
+
+// This BepInAutoPlugin attribute comes from the Hamunii.BepInEx.AutoPlugin
+// NuGet package, and it will generate the BepInPlugin attribute for you!
+// For more info, see https://github.com/Hamunii/BepInEx.AutoPlugin
+[BepInAutoPlugin]
+public partial class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
     private static int _newMaxPlayers;
@@ -28,13 +29,18 @@ public class Plugin : BaseUnityPlugin
     private static ConfigEntry<int> _configMaxPlayers;
     private static ConfigEntry<int> _configCheatExtraMarshmallows;
     private static ConfigEntry<bool> _configExtraMarshmallows;
+    private static ConfigEntry<bool> _configLateMarshmallows;
     private static int _numberOfPlayers = 1;
-    private readonly Harmony _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+    private readonly Harmony _harmony = new Harmony(Id);
+    private static List<Campfire> campfireList = new List<Campfire>();
+    private static bool isAfterAwake = false;
+    private static int vanillaMaxPlayers = 4;
+    private static Dictionary<Campfire, List<GameObject>> marshmallows = new Dictionary<Campfire, List<GameObject>>();
 
     private void Awake()
     {
         Logger = base.Logger;
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Logger.LogInfo($"Plugin {Id} is loaded!");
 
         _configMaxPlayers = Config.Bind
         (
@@ -50,10 +56,17 @@ public class Plugin : BaseUnityPlugin
             "General",
             "ExtraMarshmallows",
             true,
-            "Controls whether additional marshmallows are spawned for the extra players (Experimental)"
+            "Controls whether additional marshmallows are spawned for the extra players"
         );
         _extraMarshmallows = _configExtraMarshmallows.Value;
         
+        _configLateMarshmallows = Config.Bind
+        (
+            "General",
+            "LateJoinMarshmallows",
+            false,
+            "Controls whether additional marshmallows are spawned for players who join late (mid run), and removed for those who leave early (Experimental + Untested)"
+        );
 
         _configCheatExtraMarshmallows = Config.Bind
         (
@@ -74,17 +87,23 @@ public class Plugin : BaseUnityPlugin
         }
 
         NetworkConnector.MAX_PLAYERS = _newMaxPlayers;
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} set the Max Players to " + NetworkConnector.MAX_PLAYERS + "!");
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is patching!");
+        Logger.LogInfo($"Plugin {Id} set the Max Players to " + NetworkConnector.MAX_PLAYERS + "!");
+        Logger.LogInfo($"Plugin {Id} is patching!");
         if (_extraMarshmallows) {
-            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} extra marshmallows are enabled!");
+            Logger.LogInfo($"Plugin {Id} extra marshmallows are enabled!");
             _harmony.PatchAll(typeof(AwakePatch));
-            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} left patch enabled!");
+            Logger.LogInfo($"Plugin {Id} late marshmallows are enabled!");
+            Logger.LogInfo($"Plugin {Id} left patch enabled!");
             _harmony.PatchAll(typeof(OnPlayerLeftRoomPatch));
-            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} joined patch enabled!");
+            Logger.LogInfo($"Plugin {Id} joined patch enabled!");
             _harmony.PatchAll(typeof(OnPlayerEnteredRoomPatch));
         }
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} has patched!");
+        Logger.LogInfo($"Plugin {Id} patching end screen!");
+        _harmony.PatchAll(typeof(EndSequenceRoutinePatch));
+        _harmony.PatchAll(typeof(WaitingForPlayersUIPatch));
+        _harmony.PatchAll(typeof(EndScreenStartPatch));
+        _harmony.PatchAll(typeof(EndScreenNextPatch));
+        Logger.LogInfo($"Plugin {Id} has patched!");
     }
 
     private static List<Vector3> GetEvenlySpacedPointsAroundCampfire(int numPoints, float innerRadius, float outerRadius, Vector3 campfirePosition, Segment advanceToSegment)
@@ -118,14 +137,19 @@ public class Plugin : BaseUnityPlugin
         return points;
     }
     
-    private static void spawnMarshmallows(int number, Vector3 campfirePosition, Segment advanceToSegment)
+    private static List<GameObject> spawnMarshmallows(int number, Vector3 campfirePosition, Segment advanceToSegment)
     {
+        List<GameObject> marshmallows = new List<GameObject>();
         Item obj = SingletonAsset<ItemDatabase>.Instance.itemLookup[(ushort) 46];
-        Plugin.Logger.LogInfo((object) ("Plugin PeakUnlimited " + obj.GetName()));
+        Logger.LogInfo((object) ("Plugin PeakUnlimited " + obj.GetName()));
         obj.GetName();
-        foreach (Vector3 position in Plugin.GetEvenlySpacedPointsAroundCampfire(number, 2.5f, 3f, campfirePosition, advanceToSegment))
-            Plugin.Add(obj, position);
-        Plugin.Logger.LogInfo((object) ("Plugin PeakUnlimited added with position: " + obj.GetName()));
+        foreach (Vector3 position in GetEvenlySpacedPointsAroundCampfire(number, 2.5f, 3f, campfirePosition,
+                     advanceToSegment))
+        {
+            marshmallows.Add(Add(obj, position).gameObject);
+        }
+        Logger.LogInfo((object) ("Plugin PeakUnlimited added with position: " + obj.GetName()));
+        return marshmallows;
     }
 
     private static Vector3 SetToGround(Vector3 vector)
@@ -133,12 +157,12 @@ public class Plugin : BaseUnityPlugin
         return HelperFunctions.GetGroundPos(vector, HelperFunctions.LayerType.Default);
     }
     
-    private static void Add(Item item, Vector3 position)
+    private static Item Add(Item item, Vector3 position)
     {
         if (!PhotonNetwork.IsConnected)
-            return;
+            return null;
         Logger.LogInfo((object) string.Format("Spawn item: {0} at {1}", (object) item, (object) position));
-        PhotonNetwork.Instantiate("0_Items/" + item.name, position, Quaternion.Euler(0f, Random.Range(0f, 360f), 0f)).GetComponent<Item>();
+        return PhotonNetwork.Instantiate("0_Items/" + item.name, position, Quaternion.Euler(0f, Random.Range(0f, 360f), 0f)).GetComponent<Item>();
     }
     
     public class AwakePatch
@@ -147,38 +171,40 @@ public class Plugin : BaseUnityPlugin
         [HarmonyPostfix]
         static void Postfix(Campfire __instance)
         {
+            if (!PhotonNetwork.IsMasterClient)
+                return;
             if (__instance.gameObject.transform.parent.gameObject.name.ToLower().Contains("wings"))
             {
                 Logger.LogInfo("Skipping plane campfire...");
                 return;
             }
+            campfireList.Add(__instance);
             Logger.LogInfo("Marshmellowifying campfire...!");
-            int vanillaMaxPlayers = 4;
-            int currentPlayers = Plugin._numberOfPlayers;
-            int amountOfMarshmallowsToSpawn = currentPlayers - vanillaMaxPlayers;
+            int amountOfMarshmallowsToSpawn = _numberOfPlayers - vanillaMaxPlayers;
             if (_cheatExtraMarshmallows != 0)
             {
                 Logger.LogInfo("Adding cheatmellows!");
                 amountOfMarshmallowsToSpawn = _cheatExtraMarshmallows - vanillaMaxPlayers;
-                if (currentPlayers < vanillaMaxPlayers)
+                if (_numberOfPlayers < vanillaMaxPlayers)
                 {
-                    amountOfMarshmallowsToSpawn = _cheatExtraMarshmallows - currentPlayers;
+                    amountOfMarshmallowsToSpawn = _cheatExtraMarshmallows - _numberOfPlayers;
                 }
             }
             
             Plugin.Logger.LogInfo("Start of campfire patch!");
-            if (PhotonNetwork.IsMasterClient && (currentPlayers > vanillaMaxPlayers || _cheatExtraMarshmallows != 0))
+            if (PhotonNetwork.IsMasterClient && (_numberOfPlayers > vanillaMaxPlayers || _cheatExtraMarshmallows != 0))
             {
-                Logger.LogInfo("More than 4 players, preparing to marshmallowify! Number: " + currentPlayers);
+                Logger.LogInfo("More than 4 players, preparing to marshmallowify! Number: " + _numberOfPlayers);
                 Vector3 position = __instance.gameObject.transform.position;
                 Logger.LogInfo("Spawning " + amountOfMarshmallowsToSpawn + " marshmallows!");
-                Plugin.spawnMarshmallows(amountOfMarshmallowsToSpawn, position, __instance.advanceToSegment);
+                marshmallows.Add(__instance, spawnMarshmallows(amountOfMarshmallowsToSpawn, position, __instance.advanceToSegment));
                 Logger.LogInfo("End of campfire patch!");
             }
             else
             {
                 Logger.LogInfo("Not enough players for campfire patch or not host!");
             }
+            isAfterAwake = true;
         }
     }
     
@@ -190,6 +216,18 @@ public class Plugin : BaseUnityPlugin
         {
             _numberOfPlayers++;
             Logger.LogInfo("Someone has joined the room! Number: " + _numberOfPlayers + "/" + NetworkConnector.MAX_PLAYERS);
+            //Add a marshmallow at each campfire for the new player
+            if (!_configLateMarshmallows.Value)
+                return;
+            if (isAfterAwake && PhotonNetwork.IsMasterClient && _numberOfPlayers > vanillaMaxPlayers && _cheatExtraMarshmallows == 0)
+            {
+                foreach (Campfire campfire in campfireList)
+                {
+                    Vector3 position = campfire.gameObject.transform.position;
+                    Logger.LogInfo("Spawning a marshmallow!");
+                    marshmallows[campfire].Add(spawnMarshmallows(1, position, campfire.advanceToSegment)[0]);
+                }
+            }
         }
     }
     
@@ -205,6 +243,121 @@ public class Plugin : BaseUnityPlugin
                 _numberOfPlayers = 0;
             }
             Logger.LogInfo("Someone has left the room! Number: " + _numberOfPlayers + "/" + NetworkConnector.MAX_PLAYERS);
+            if (!_configLateMarshmallows.Value)
+                return;
+            if (isAfterAwake && PhotonNetwork.IsMasterClient && _numberOfPlayers >= vanillaMaxPlayers && _cheatExtraMarshmallows == 0)
+            {
+                Logger.LogInfo("Removing a marshmallow!");
+                foreach (Campfire campfire in campfireList)
+                {
+                    Logger.LogInfo("Removing a marshmallow! " +  marshmallows[campfire].Count);
+                    Logger.LogInfo("Removing a marshmallow! " +  marshmallows[campfire][0].gameObject.name);
+                    Destroy(marshmallows[campfire][0]);
+                    marshmallows[campfire].RemoveAt(0);
+                    Logger.LogInfo("Removing a marshmallow! " +  marshmallows[campfire].Count);
+                    Logger.LogInfo("Removing a marshmallow! " +  marshmallows[campfire][0].gameObject.name);
+                }
+            }
+        }
+    }
+
+    public class WaitingForPlayersUIPatch
+    {
+        [HarmonyPatch(typeof(WaitingForPlayersUI), "Update")]
+        [HarmonyPrefix]
+        static void Prefix(WaitingForPlayersUI __instance)
+        {
+            if (__instance.scoutImages.Length >= Character.AllCharacters.Count)
+                return;
+            var newScoutImages = new Image[Character.AllCharacters.Count];
+            Image original = __instance.scoutImages[0];
+            for (int i = 0; i < Character.AllCharacters.Count; i++)
+            {
+                if (i < vanillaMaxPlayers)
+                {
+                    newScoutImages[i] = __instance.scoutImages[i];
+                }
+                else
+                {
+                    newScoutImages[i] = Instantiate(original, original.transform.parent);
+                }
+            }
+            __instance.scoutImages = newScoutImages;
+        }
+    }
+    
+    public class EndScreenNextPatch
+    {
+        [HarmonyPatch(typeof (EndScreen), "Next")]
+        [HarmonyPostfix]
+        private static void PostFix()
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Singleton<PeakHandler>.Instance.EndScreenComplete();
+            }
+        }
+    }
+
+    public class EndScreenStartPatch
+    {
+        [HarmonyPatch(typeof(EndScreen), "Start")]
+        [HarmonyPrefix]
+        static void Prefix(EndScreen __instance)
+        {
+            if (Character.AllCharacters.Count <= vanillaMaxPlayers)
+                return;
+
+            var newScoutWindows = new EndScreenScoutWindow[Character.AllCharacters.Count];
+            var newScouts = new Image[Character.AllCharacters.Count];
+            var newScoutsAtPeak = new Image[Character.AllCharacters.Count];
+            var newOldPip = new Image[Character.AllCharacters.Count];
+            var newScoutLines = new Transform[Character.AllCharacters.Count];
+
+            for (int i = 0; i < Character.AllCharacters.Count; i++)
+            {
+                //Don't do anything to the original ones
+                bool withinExisting = i < __instance.scouts.Length;
+                if (!withinExisting)
+                {
+                    newScoutWindows[i] = Instantiate(__instance.scoutWindows[0], __instance.scoutWindows[i].transform.parent);
+                    newScouts[i] = Instantiate(__instance.scouts[0], __instance.scoutWindows[i].transform.parent);
+                    newScoutsAtPeak[i] = Instantiate(__instance.scoutsAtPeak[0], __instance.scoutWindows[i].transform.parent);
+                    newOldPip[i] = Instantiate(__instance.oldPip[0], __instance.scoutWindows[i].transform.parent);
+                    newScoutLines[i] = Instantiate(__instance.scoutLines[0], __instance.scoutWindows[i].transform.parent);
+                }
+                else
+                {
+                    newScoutWindows[i] = __instance.scoutWindows[i];
+                    newScouts[i] = __instance.scouts[i];
+                    newScoutsAtPeak[i] = __instance.scoutsAtPeak[i];
+                    newOldPip[i] = __instance.oldPip[i];
+                    newScoutLines[i] = __instance.scoutLines[i];
+                }
+            }
+
+            //Reassign arrays with new ones
+            __instance.scoutWindows = newScoutWindows;
+            __instance.scouts = newScouts;
+            __instance.scoutsAtPeak = newScoutsAtPeak;
+            __instance.oldPip = newOldPip;
+            __instance.scoutLines = newScoutLines;
+        }
+    }
+
+    public class EndSequenceRoutinePatch
+    {
+        [HarmonyPatch(typeof(EndScreen), "EndSequenceRoutine")]
+        [HarmonyPostfix]
+        static void Postfix(EndScreen __instance)
+        {
+            for (int i = 4; i < Character.AllCharacters.Count; i++)
+            {
+                Logger.LogInfo("Deactivating an end screen");
+                //Don't display the extra names since it blocks the chart
+                Destroy(__instance.scoutWindows[i].gameObject);
+                Logger.LogInfo("Deleted an end screen");
+            }
         }
     }
 }
