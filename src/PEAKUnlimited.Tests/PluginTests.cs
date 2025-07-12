38c5ae77b4b42e0346476c12619d9de7b79e1fc6
Arik <arikchowdhury@gmail.com>
2025-07-12 19:20:15 -0400

    add tests to project

--- src/PEAKUnlimited.Tests/PluginTests.cs
+++ src/PEAKUnlimited.Tests/PluginTests.cs
@@ -1,15 +1,11 @@
-// <copyright file="PluginTests.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace PEAKUnlimited.Tests
 {
-    using System.Collections.Generic;
-    using System.Reflection;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using UnityEngine;
-    using UnityEngine.UI;
-
     [TestClass]
     public class PluginTests
     {
@@ -31,16 +27,15 @@ namespace PEAKUnlimited.Tests
             float innerRadius = 2.5f;
             float outerRadius = 3.0f;
 
-            var points = Plugin.GetEvenlySpacedPointsAroundCampfire(numPoints, innerRadius, outerRadius, campfirePosition, default(Segment));
+            var points = Plugin.GetEvenlySpacedPointsAroundCampfire(numPoints, innerRadius, outerRadius, campfirePosition, new Segment());
 
             Assert.AreEqual(numPoints, points.Count, "Should return exactly the requested number of points");
-
+            
             // Test that points are within expected radius
             foreach (var point in points)
             {
                 float distance = Vector3.Distance(campfirePosition, point);
-                Assert.IsTrue(
-                    distance >= innerRadius && distance <= outerRadius,
+                Assert.IsTrue(distance >= innerRadius && distance <= outerRadius, 
                     $"Point {point} should be within radius {innerRadius}-{outerRadius}, but was {distance}");
             }
         }
@@ -50,11 +45,11 @@ namespace PEAKUnlimited.Tests
         {
             // Test player count validation logic
             int vanillaMaxPlayers = 4;
-
+            
             // Test valid player counts
             Assert.IsTrue(Plugin.ValidatePlayerCount(5, vanillaMaxPlayers), "5 players should be valid");
             Assert.IsTrue(Plugin.ValidatePlayerCount(20, vanillaMaxPlayers), "20 players should be valid");
-
+            
             // Test invalid player counts
             Assert.IsFalse(Plugin.ValidatePlayerCount(0, vanillaMaxPlayers), "0 players should be invalid");
             Assert.IsFalse(Plugin.ValidatePlayerCount(-1, vanillaMaxPlayers), "Negative players should be invalid");
@@ -85,12 +80,12 @@ namespace PEAKUnlimited.Tests
             // Test that end screen arrays are properly expanded
             int originalCount = 4;
             int newCount = 8;
-
+            
             var originalArray = new Image[originalCount];
             var expandedArray = Plugin.ExpandArrayForExtraPlayers(originalArray, newCount);
-
+            
             Assert.AreEqual(newCount, expandedArray.Length, "Array should be expanded to new count");
-
+            
             // Test that original elements are preserved
             for (int i = 0; i < originalCount; i++)
             {
@@ -104,11 +99,11 @@ namespace PEAKUnlimited.Tests
             // Test marshmallow spawn calculations
             int vanillaMaxPlayers = 4;
             int currentPlayers = 6;
-
+            
             int expectedMarshmallows = currentPlayers - vanillaMaxPlayers; // Should be 2
             int actualMarshmallows = Plugin.CalculateExtraMarshmallows(currentPlayers, vanillaMaxPlayers, 0);
-
-            Assert.AreEqual(expectedMarshmallows, actualMarshmallows,
+            
+            Assert.AreEqual(expectedMarshmallows, actualMarshmallows, 
                 $"Should spawn {expectedMarshmallows} extra marshmallows for {currentPlayers} players");
         }
 
@@ -118,12 +113,12 @@ namespace PEAKUnlimited.Tests
             // Test backpack spawn calculations
             int vanillaMaxPlayers = 4;
             int currentPlayers = 8;
-
+            
             // 25% chance per extra player, so 4 extra players = 1 backpack
             int expectedBackpacks = (currentPlayers - vanillaMaxPlayers) / 4; // Should be 1
             int actualBackpacks = Plugin.CalculateExtraBackpacks(currentPlayers, vanillaMaxPlayers);
-
-            Assert.AreEqual(expectedBackpacks, actualBackpacks,
+            
+            Assert.AreEqual(expectedBackpacks, actualBackpacks, 
                 $"Should spawn {expectedBackpacks} extra backpacks for {currentPlayers} players");
         }
     }
@@ -132,14 +127,12 @@ namespace PEAKUnlimited.Tests
     public class EndScreen
     {
         private Image[] oldPip = new Image[4];
-        public Image[] Scouts = new Image[4];
-        public Image[] ScoutsAtPeak = new Image[4];
-        public EndScreenScoutWindow[] ScoutWindows = new EndScreenScoutWindow[4];
-        public Transform[] ScoutLines = new Transform[4];
+        public Image[] scouts = new Image[4];
+        public Image[] scoutsAtPeak = new Image[4];
+        public EndScreenScoutWindow[] scoutWindows = new EndScreenScoutWindow[4];
+        public Transform[] scoutLines = new Transform[4];
     }
 
     // Mock classes for testing
-    public class EndScreenScoutWindow
-    {
-    }
-}
+    public class EndScreenScoutWindow { }
+} 
